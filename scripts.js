096e56fb4cbecf546ba77792d5291379cc49071e
simasgabriel <45041309+simasgabriel@users.noreply.github.com>
2023-05-16 11:42:53 -0300

    finished local storage configuration

--- scripts.js
+++ scripts.js
@@ -42,9 +42,18 @@ const todosApp={
 
 },
 
+},
+created(){
+    this.todos=localStorage.getItem('todos') ? JSON.parse(localStorage.getItem("todos")):this.todos;
 
+},
+updated(){
+    localStorage.setItem("todos",JSON.stringify(this.todos));
 }
 }
 
 
 Vue.createApp(todosApp).mount('#app');
+
+
+
